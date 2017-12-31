6f1c3633f51ee1b7b194a5fe5da3bedf9a86cee9
Michael <Mike@michaels-mbp-2.home>
2017-12-31 16:04:31 -0600

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+//hamburger jQuery code
+(function () {
+	$('#hamburger').on('click', function() {
+		$('#bun').toggleClass('animate');
+    $('#cheese').toggleClass('animate');
+    $('#patty').toggleClass('animate');
+    $('#toggleNav').toggleClass('animate');
+    $('#navLogo').toggleClass('animate');
+    $('#menuLine1').toggleClass('animate');
+    $('#menuLine2').toggleClass('animate');
+    $('#nav1').toggleClass('animate');
+    $('#nav2').toggleClass('animate');
+    $('#nav3').toggleClass('animate');
+    $('#nav4').toggleClass('animate');
+    $('#withLove').toggleClass('animate');
+	})
+})();
