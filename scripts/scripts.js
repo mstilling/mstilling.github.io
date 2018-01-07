e6ae234a4519842e7f0760d85aff5f5ede73c32b
Michael <Mike@michaels-mbp-2.home>
2018-01-07 02:34:10 -0600

    wrong folder grab

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,9 @@
-$("#menu").click(function(){
-  $("#menu").toggleClass("animate");
-  $("#menuBar1").toggleClass("animate");
-  $("#menuBar2").toggleClass("animate");
-  $("#menuBar3").toggleClass("animate");
-  $("#toggleNav").toggleClass("animate");
-  $("nav").toggleClass("animate");
-  $("#logo").toggleClass("animate");
-  $(".project").toggleClass("animate");
-});
+//menu jQuery code
+(function () {
+	$('#menuButton').on('click', function() {
+    $('#menu').toggleClass('animate');
+		$('#menuBar1').toggleClass('animate');
+    $('#menuBar2').toggleClass('animate');
+		$('.temp').toggleClass('animate');
+	})
+})();
