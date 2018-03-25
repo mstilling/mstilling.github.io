ab181d17ad18b8e49a90fa1d5e2f0d4df33c764c
Michael <Mike@michaels-mbp-2.home>
2018-03-25 14:34:59 -0500

    complete redesign

--- scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-//menu jQuery code
-(function () {
-	$('#menuButton').on('click', function() {
-    $('#menu').toggleClass('animate');
-		$('#menuBar1').toggleClass('animate');
-    $('#menuBar2').toggleClass('animate');
-		$('.temp').toggleClass('animate');
-	})
-})();
